658f6708cfd208337e0f9bace70e3f16b1ae91b1
Luis_Hernandez <luizhzgz@gmail.com>
2025-03-10 19:34:24 -0600

    primer commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//los comentarios se añaden con dos diagonales// 
+/* Los comentarios se hacen con alt+ shit + a */
+
+//Para imprimir en pantalla usamos console.log()//
+console.log("Hola ucamp!");
+
